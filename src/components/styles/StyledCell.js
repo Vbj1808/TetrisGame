2882d04147a993caef76f014afbac1ef39e019ad
Vbj <ijalab1808@gmail.com>
2020-05-24 10:29:50 +0530

    Ready To Play

--- src/components/styles/StyledCell.js
+++ src/components/styles/StyledCell.js
@@ -8,4 +8,4 @@ export const StyledCell = styled.div`
     border-right-color: rgba(${props => props.color}, 1);
     border-top-color: rgba(${props => props.color}, 1);
     border-left-color: rgba(${props => props.color}, 0.3);
-`
+`;
